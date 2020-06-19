4ae2524869f3e6dd8a2d34500d940b27ca4fb6d5
zhangyirong123 <525386154@qq.com>
2020-06-19 17:22:59 +0800

    完成测试1A3B

--- src/main/java/com/twschool/practice/GuessNumber.java
+++ src/main/java/com/twschool/practice/GuessNumber.java
@@ -1,14 +1,27 @@
 package com.twschool.practice;
 
+import org.apache.commons.lang.ArrayUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class GuessNumber {
 
 
     public static String checkResult(String userNumber, String gameNumber) {
 
-        if (userNumber.equals(gameNumber)) {
-            return "4A0B";
+        int A = 0;
+        int B = 0;
+        List<String> userNumberList = Arrays.asList(userNumber.split(" "));
+        List<String> gameNumberList = Arrays.asList(gameNumber.split(" "));
+        for (int index = 0; index < userNumberList.size(); index++) {
+            if (userNumberList.get(index).equals(gameNumberList.get(index))) {
+                A++;
+            } else if (gameNumberList.contains(userNumberList.get(index))) {
+                B++;
+                System.out.println(B);
+            }
         }
-        return "fail";
-
+        return A + "A" + B + "B";
     }
 }
--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -1,6 +1,7 @@
 package com.twschool.practice;
 
 import org.junit.Test;
+import org.mockito.stubbing.Answer;
 
 import static org.junit.Assert.assertEquals;
 
@@ -8,12 +9,24 @@ public class GuessNumberTest {
     @Test
     public void Should_Return_4A0B_when_given_gameAnswer_1234_userAnswer_1234() {
         //given
-        String userNumber = "1234";
-        String gameNumber = "1234";
+        String userNumber = "1 2 3 4";
+        String gameNumber = "1 2 3 4";
 
         //when
         String result = GuessNumber.checkResult(userNumber, gameNumber);
         //then
         assertEquals("4A0B", result);
+    }
+
+    @Test
+    public void Should_Return_4A0B_when_given_gameAnswer_1234_userAnswer_1432() {
+        //given
+        String userNumber = "1 4 3 2";
+        String gameNumber = "1 2 4 3";
+
+        //when
+        String result = GuessNumber.checkResult(userNumber, gameNumber);
+        //then
+        assertEquals("1A3B", result);
     }
 }
