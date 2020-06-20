8df0925e9a68c771a40f19fbbd8adb9c040666a0
zhangyirong123 <525386154@qq.com>
2020-06-20 10:05:43 +0800

    完成Success案例

--- src/main/java/com/twschool/practice/GuessNumberMain.java
+++ src/main/java/com/twschool/practice/GuessNumberMain.java
@@ -1,10 +1,17 @@
 package com.twschool.practice;
 
 public class GuessNumberMain {
-    public GuessNumberMain(String gameNumber) {
+
+    private GuessNumber guessNumber;
+
+    public GuessNumberMain(GuessNumber guessNumber) {
+        this.guessNumber = guessNumber;
     }
 
     public String checkResult(String userNumber) {
+        if ("4A0B".equals(guessNumber.check(userNumber))) {
+            return "Success";
+        }
         return "TryAgain";
     }
 }
--- src/test/java/com/twschool/practice/GuessNumberMainTest.java
+++ src/test/java/com/twschool/practice/GuessNumberMainTest.java
@@ -10,10 +10,23 @@ public class GuessNumberMainTest {
     public void Should_Return_TryAgain_when_given_gameAnswer_1234_userAnswer_1432() {
         //given
         String userNumber = "1 4 3 2";
-        GuessNumberMain guessNumberMain = new GuessNumberMain("1 2 4 3");
+        GuessNumber guessNumber = new GuessNumber("1 2 3 4");
+        GuessNumberMain guessNumberMain = new GuessNumberMain(guessNumber);
         //when
         String result = guessNumberMain.checkResult(userNumber);
         //then
         assertEquals("TryAgain", result);
+    }
+
+    @Test
+    public void Should_Return_Success_when_given_gameAnswer_1234_userAnswer_1234() {
+        //given
+        String userNumber = "1 2 3 4";
+        GuessNumber guessNumber = new GuessNumber("1 2 3 4");
+        GuessNumberMain guessNumberMain = new GuessNumberMain(guessNumber);
+        //when
+        String result = guessNumberMain.checkResult(userNumber);
+        //then
+        assertEquals("Success", result);
     }
 }
