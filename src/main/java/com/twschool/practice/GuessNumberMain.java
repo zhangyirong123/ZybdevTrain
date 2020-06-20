f7da7e1534cb2b6d180b3f4b31a9af454e8239c6
zhangyirong123 <525386154@qq.com>
2020-06-20 15:54:25 +0800

    GuessNumber

--- src/main/java/com/twschool/practice/GuessNumberMain.java
+++ src/main/java/com/twschool/practice/GuessNumberMain.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class GuessNumberMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("欢迎来到猜字游戏");
+        System.out.println("欢迎来到猜字游戏！");
 
         Generator generator = new Generator();
         String gameNumber = generator.getGameNumber();
@@ -15,15 +15,15 @@ public class GuessNumberMain {
             String userAnswer = scan.nextLine();
             guessNumber.checkResult(userAnswer);
             if (GameStatus.Success.equals(guessNumber.getGameStatus())) {
-                System.out.println("恭喜您，猜对了！");
+                System.out.println("恭喜您，成功了！");
                 break;
             }
             if (GameStatus.Fail.equals(guessNumber.getGameStatus())) {
-                System.out.println("不好意思，失败了，再来一局吧！");
+                System.out.println("不好意思，失败了，您可以重新开始！");
                 break;
             }
             if (GameStatus.TryAgain.equals(guessNumber.getGameStatus())) {
-                System.out.println("再猜一下!");
+                System.out.println("再试一次！");
             }
         }
 
